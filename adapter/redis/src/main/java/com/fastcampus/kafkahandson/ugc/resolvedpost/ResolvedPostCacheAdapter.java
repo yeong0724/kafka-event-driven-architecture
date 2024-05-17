c5dd7189549600a25aa62986f8b7cf2e9e7d44bb
KJY <kid4211@gmail.com>
2024-05-17 16:13:29 +0900

    Kafka + Redis - 캐싱에 실시간성 부여하도록 시스템 개선 (1)

---
+++ adapter/redis/src/main/java/com/fastcampus/kafkahandson/ugc/resolvedpost/ResolvedPostCacheAdapter.java
@@ -0,0 +1,57 @@
+package com.fastcampus.kafkahandson.ugc.resolvedpost;
+
+import com.fastcampus.kafkahandson.ugc.CustomObjectMapper;
+import com.fastcampus.kafkahandson.ugc.port.ResolvedPostCachePort;
+import com.fastcampus.kafkahandson.ugc.post.model.ResolvedPost;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class ResolvedPostCacheAdapter implements ResolvedPostCachePort {
+    private static final String KEY_PREFIX = "resolved_post:v1:";
+    private static final Long EXPIRE_SECONDS = 60L * 60L * 2L;  // 2시간
+    private final CustomObjectMapper objectMapper = new CustomObjectMapper();
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public ResolvedPostCacheAdapter(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void set(ResolvedPost resolvedPost) {
+        String jsonString;
+        try {
+            jsonString = objectMapper.writeValueAsString(resolvedPost);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        redisTemplate.opsForValue().set(
+            this.generateCacheKey(resolvedPost.getId()),
+            jsonString,
+            Duration.ofSeconds(EXPIRE_SECONDS) // cache 의 유효기간
+        );
+    }
+
+    @Override
+    public ResolvedPost get(Long postId) {
+        String jsonString = redisTemplate.opsForValue().get(this.generateCacheKey(postId));
+
+        if (jsonString == null) {
+            return null;
+        }
+
+        try {
+            return objectMapper.readValue(jsonString, ResolvedPost.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateCacheKey(Long postId) {
+        return KEY_PREFIX + postId;
+    }
+}
--- usecase/post-resolving-help-usecase/src/main/java/com/fastcampus/kafkahandson/ugc/PostResolvingHelpService.java
+++ usecase/post-resolving-help-usecase/src/main/java/com/fastcampus/kafkahandson/ugc/PostResolvingHelpService.java
@@ -2,26 +2,37 @@ package com.fastcampus.kafkahandson.ugc;
 
 import com.fastcampus.kafkahandson.ugc.port.MetadataPort;
 import com.fastcampus.kafkahandson.ugc.port.PostPort;
+import com.fastcampus.kafkahandson.ugc.port.ResolvedPostCachePort;
 import com.fastcampus.kafkahandson.ugc.post.model.Post;
 import com.fastcampus.kafkahandson.ugc.post.model.ResolvedPost;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class PostResolvingHelpService implements PostResolvingHelpUsecase {
     private final PostPort postPort; // 핵심 원천 데이터
-
     private final MetadataPort metadataPort; // 부가 데이터
+    private final ResolvedPostCachePort resolvedPostCachePort; // 캐시 데이터
+
+    @Autowired
+    public PostResolvingHelpService(PostPort postPort, MetadataPort metadataPort, ResolvedPostCachePort resolvedPostCachePort) {
+        this.postPort = postPort;
+        this.metadataPort = metadataPort;
+        this.resolvedPostCachePort = resolvedPostCachePort;
+    }
 
     @Override
     public ResolvedPost resolvePostById(Long postId) {
-        ResolvedPost resolvedPost = null;
+        ResolvedPost resolvedPost = resolvedPostCachePort.get(postId);
+
+        System.out.println("[PostResolvingHelpService] resolvePostById :: resolvedPost = " + resolvedPost);
+        if (resolvedPost != null) {
+            return resolvedPost;
+        }
 
         Post post = postPort.findById(postId);
-
         if (post != null) {
             String userName = metadataPort.getUserNameByUserId(post.getUserId());
             String categoryName = metadataPort.getCategoryNameByCategoryId(post.getCategoryId());
@@ -32,6 +43,8 @@ public class PostResolvingHelpService implements PostResolvingHelpUsecase {
                         userName,
                         categoryName
                 );
+
+                resolvedPostCachePort.set(resolvedPost);
             }
         }
 
